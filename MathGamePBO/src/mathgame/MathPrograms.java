04d6e4f5a84c9a5a6857a254603c238082aa8f7c
brilyanramadhan <56918233+brilyanramadhan@users.noreply.github.com>
2019-11-13 21:21:43 +0700

    replace from if to switch method

--- MathGamePBO/src/mathgame/MathPrograms.java
+++ MathGamePBO/src/mathgame/MathPrograms.java
@@ -22,48 +22,49 @@ public class MathPrograms {
         System.out.println("3. Keluar");
         System.out.println("Pilih no menu :");
         pilihan_menu = in.nextInt();
-        if (pilihan_menu == 1) {
-            System.out.println("****************************");
-            System.out.println("     Math - Penjumlahan     ");
-            System.out.println("****************************");
-            System.out.println("1. Lanjut");
-            System.out.println("2. Ulang");
-            System.out.println("Pilih no menu :");
-            pilihan_menu_dua = in.nextInt();
-            if (pilihan_menu_dua == 1) {
+        switch (pilihan_menu) {
+            case 1:
                 System.out.println("****************************");
                 System.out.println("     Math - Penjumlahan     ");
                 System.out.println("****************************");
-                System.out.println("Masukkan Level Terakhir : ");
-                last_level = in.nextInt();
-                System.out.println("Masukkan Score Terakhir : ");
-                last_score = in.nextInt();
-                cplus.Play(last_score, last_level);
-            } else if (pilihan_menu_dua == 2) {
-                cplus.Play();
-            }
-        } else if (pilihan_menu == 2) {
-            System.out.println("****************************");
-            System.out.println("     Math - Pengurangan     ");
-            System.out.println("****************************");
-            System.out.println("1. Lanjut");
-            System.out.println("2. Ulang");
-            System.out.println("Pilih no menu :");
-            pilihan_menu_dua = in.nextInt();
-            if (pilihan_menu_dua == 1) {
+                System.out.println("1. Lanjut");
+                System.out.println("2. Ulang");
+                System.out.println("Pilih no menu :");
+                pilihan_menu_dua = in.nextInt();
+                if (pilihan_menu_dua == 1) {
+                    System.out.println("****************************");
+                    System.out.println("     Math - Penjumlahan     ");
+                    System.out.println("****************************");
+                    System.out.println("Masukkan Level Terakhir : ");
+                    last_level = in.nextInt();
+                    System.out.println("Masukkan Score Terakhir : ");
+                    last_score = in.nextInt();
+                    cplus.Play(last_score, last_level);
+                } else if (pilihan_menu_dua == 2) {
+                    cplus.Play();
+                }   break;
+            case 2:
                 System.out.println("****************************");
-                System.out.println("     Math - Penjumlahan     ");
+                System.out.println("     Math - Pengurangan     ");
                 System.out.println("****************************");
-                System.out.println("Masukkan Level Terakhir : ");
-                last_level = in.nextInt();
-                System.out.println("Masukkan Score Terakhir : ");
-                last_score = in.nextInt();
-                cmin.Play(last_score, last_level);
-            } else if (pilihan_menu_dua == 2) {
-                cmin.Play();
-            }
-        } else {
-
+                System.out.println("1. Lanjut");
+                System.out.println("2. Ulang");
+                System.out.println("Pilih no menu :");
+                pilihan_menu_dua = in.nextInt();
+                if (pilihan_menu_dua == 1) {
+                    System.out.println("****************************");
+                    System.out.println("     Math - Penjumlahan     ");
+                    System.out.println("****************************");
+                    System.out.println("Masukkan Level Terakhir : ");
+                    last_level = in.nextInt();
+                    System.out.println("Masukkan Score Terakhir : ");
+                    last_score = in.nextInt();
+                    cmin.Play(last_score, last_level);
+                } else if (pilihan_menu_dua == 2) {
+                    cmin.Play();
+                }   break;
+            default:
+                break;
         }
 
     }
